3ca9f163b09508aff637c41a400958868e0ddd3d
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-07 23:33:35 +0000

    Fix views, imports, templates, and urls to pass checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+from .models import Library
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
@@ -7,7 +8,8 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
+from django.views.generic.detail import DetailView  # ✅ Required
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
