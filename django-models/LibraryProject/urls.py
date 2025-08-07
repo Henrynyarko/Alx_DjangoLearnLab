ec19ab0d24e466c4c17665058cf7370ccd47fd3c
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-07 23:29:45 +0000

    Fix template folder structure and file names

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  # function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # class-based view
 ]
+
+
+
