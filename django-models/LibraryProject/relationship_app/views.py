f06c8dd4a95c024f83828bfa5227b73b810e8f18
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-07 23:15:51 +0000

    Fix imports and structure for DetailView, templates, and URLs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
@@ -12,6 +12,8 @@ def list_books(request):
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 # ✅ Class-based view to show details of a specific library
+from django.views.generic.detail import DetailView
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
