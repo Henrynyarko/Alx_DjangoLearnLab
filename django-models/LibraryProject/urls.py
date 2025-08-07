80a6c2876511fb6be172a31c78cce3fecaaf5fc6
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-07 23:19:40 +0000

    Add app urls.py and templates for books and library views

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # includes the app URLs
+]
+
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
