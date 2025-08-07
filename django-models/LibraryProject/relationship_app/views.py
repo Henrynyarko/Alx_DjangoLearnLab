673bc401093181d13df1474559a5e711e03893d5
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-07 23:24:32 +0000

    Fix template folder structure and file names

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,15 +6,17 @@ from django.views.generic import DetailView
 from .models import Library, Book   # <--- this line is good. But if needed, split it:
 from .models import Library  # <-- Make sure this line is literally there
 
-# ✅ Function-based view to list all books
+# from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# ✅ Class-based view to show details of a specific library
-from django.views.generic.detail import DetailView
-
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
