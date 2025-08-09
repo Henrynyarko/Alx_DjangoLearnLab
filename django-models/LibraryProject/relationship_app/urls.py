0b2d23b1c2fd723241428fd6375e695e89ec55ec
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-09 23:06:17 +0000

    Fix auth templates and authentication flow

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -59,4 +59,12 @@ urlpatterns = [
     
 ]
 
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register
 
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
