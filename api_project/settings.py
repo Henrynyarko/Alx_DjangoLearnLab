26f408d799b64fd8b832a6ccbcf8dde1db5deac4
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-10 23:15:00 +0000

    Initial setup with Django REST Framework and Book model

--- api_project/settings.py
+++ api_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'rest_framework',  # 👈 Add this
-    'api',     
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
