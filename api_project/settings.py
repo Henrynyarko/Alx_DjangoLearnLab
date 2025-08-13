7d80b6f70007d8b4c29ee8452d88558dcaa9a565
Ernest Nyarko <127901738+Henrynyarko@users.noreply.github.com>
2025-08-13 15:33:46 +0000

    Update settings.py

--- api_project/settings.py
+++ api_project/settings.py
@@ -26,8 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',  # ✅ Added
-    'api',             # ✅ Added
+    'rest_framework',  
+    'api',             
 ]
 
 MIDDLEWARE = [
