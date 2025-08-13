a609455ee86227c4102f6402bf9b19fa5258c5a1
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-13 14:55:04 +0000

    Initial Django project with DRF and Book model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    published_date = models.DateField()
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author}"
