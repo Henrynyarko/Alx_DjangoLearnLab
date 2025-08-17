21808986ff13707c526f5981f166274af79ef1b8
Ernest Nyarko <127901738+Henrynyarko@users.noreply.github.com>
2025-08-17 23:04:44 +0000

    Delete api directory

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import BookListCreateView, home
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('books/', BookListCreateView.as_view(), name='book-list'),
-]
