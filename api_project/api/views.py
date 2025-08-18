24455c7e37e9d39e7433bb3d0f9665cd540016cc
Henry Nyarko <henryeknyarko@gmail.com>
2025-08-18 00:23:42 +0000

    Fix project structure and settings module

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateView(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-# Create your views here.
