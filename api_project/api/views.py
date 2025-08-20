a68f10a29a734aea8bbbf817c2442b5d12a0f50c
Ernest Nyarko <127901738+Henrynyarko@users.noreply.github.com>
2025-08-20 11:10:43 +0000

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,15 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard actions (CRUD) for the Book model
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
